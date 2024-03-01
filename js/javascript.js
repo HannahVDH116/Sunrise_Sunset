1707fdfb140fde6d5594898fe3e847704f4c58c0
HannahVDH116 <hannah.vanderhelm@gmail.com>
2024-03-02 10:30:11 +1100

    Debug 12/02/2024
    First debug of JS, with limited API fetch. Attempt to get API working before adding functionality to different search buttons.

--- js/javascript.js
+++ js/javascript.js
@@ -1,38 +1,41 @@
-const sunriseButton = document.getElementById('sunriseButton')
-const errorMessage = document.getElementById('errorMessage')
-const sunCard = document.getElementById('sunCard')
-const sunriseTime = document.getElementById('sunriseTime')
-const sunsetTime = document.getElementById('sunsetTime')
+document.addEventListener('DOMContentLoaded', function() {
+  const sunriseButton = document.getElementById('sunriseButton')
+  const errorMessage = document.getElementById('errorMessage')
+  const sunCard = document.getElementById('sunCard')
+  const sunriseTime = document.getElementById('sunriseTime')
+  const sunsetTime = document.getElementById('sunsetTime')
 
 
-errorMessage.style.display = "none"
-sunCard.style.display = "none"
+  errorMessage.style.display = "none"
+  sunCard.style.display = "none"
 
-sunriseButton.addEventListener("click", searchSun)
+  sunriseButton.addEventListener("click", searchSun)
 
-async function searchSun (){
-  try{
-    const res = await fetch('https://api.sunrise-sunset.org/json?lat=-42.87936000&lng=147.32941000&date=today&tzid=Australia/Hobart')
-    const json = await res.json()
-    console.log(json);
-    errorMessage.style.display = "none"
-    displayData(json);
-  } catch(error) {
-    errorMessage.style.display = "block"
-    errorMessage.innerHTML = "No-one's Home! (The API could not be reached!) 😢 "
+  async function searchSun (){
+    try{
+      const res = await fetch('https://api.sunrise-sunset.org/json?lat=-42.87936000&lng=147.32941000&date=today&tzid=Australia/Hobart')
+      const json = await res.json()
+      console.log(json);
+
+      if(json.status === "okay"){
+        const {sunrise, sunset} = json.results
+        errorMessage.style.display = "none"
+        displayData(sunrise, sunset);
+        } else {
+          errorMessage.style.display = "block"
+          errorMessage.innerHTML = "No sunshine for You! (Times not Found!) 🫥 "
+        } 
+    } catch(error) {
+      errorMessage.style.display = "block"
+      errorMessage.innerHTML = "No-one's Home! (The API could not be reached!) 😢 "
+    }
+    
   }
-  
-}
-
-function displayData(results){
-  console.log(results.Error)
-  if(results.Error){
-    errorMessage.style.display = "block"
-    errorMessage.innerHTML = "No sunshine for You! (Times not Found!) 🫥 "
-    return;
+  function displayData(sunrise, sunset){
+      sunCard.style.display = "block"
+  sunriseTime.innerHTML = results.sunrise
+  sunsetTime.innerHTML = results.sunset
   }
-  
-sunCard.style.display = "block"
-sunriseTime.innerHTML = results.sunrise
-sunsetTime.innerHTML = results.sunset
-}
+})
+
+searchSun();
